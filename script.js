8bfd25960f1eafca4de6ff3270afc7f4aa355a8d
Antoniue <Verstiplayingtm@gmail.com>
2022-09-11 11:56:55 +0300

    fixed restart button functionality and resized, added event listeners to the grids

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+// factory function for players
+const playerFactory = (symbol) => {
+    this.symbol = symbol;
+    return {symbol};
+}
+
 // gameBoard module
 const gameBoard = (() => {
-    let row1board = ['x','',''];
-    let row2board = ['','x','o'];
-    let row3board = ['o','','x'];
+    let row1board = ['','',''];
+    let row2board = ['','',''];
+    let row3board = ['','',''];
     const getBoard = () => {
         return [row1board,row2board,row3board];
     }
+
     const restartBoard = () => {
         row1board = ['','',''];
         row2board = ['','',''];
@@ -15,23 +22,19 @@ const gameBoard = (() => {
     const butt = document.querySelector('.restartButton');
     butt.addEventListener(
         'click', 
-        () => restartBoard()
-    );
-
-    return {getBoard };
+        () => {restartBoard(); console.log('restart');}
+);
+    return {getBoard};
 }
 )();
-
-// factory function for players
-const playerFactory = (symbol) => {
-    this.symbol = symbol;
-}
 
 // gameFlow module
 const gameFlow = (() => {
     let row1 = document.querySelector('.row1');
     let row2 = document.querySelector('.row2');
     let row3 = document.querySelector('.row3');
+    const player1 = playerFactory('X');
+    const player2 = playerFactory('O');
     const render = () => {
         board = gameBoard.getBoard();
         row1.innerHTML = '';
@@ -39,15 +42,27 @@ const gameFlow = (() => {
         row3.innerHTML = '';
         for(let index = 0; index < 3; index++)
         {
-            row1.innerHTML += '<div class="grid grid1'+(index+1)+'">'+board[0][index]+'</div>'
-            row2.innerHTML += '<div class="grid grid2'+(index+1)+'">'+board[1][index]+'</div>'
-            row3.innerHTML += '<div class="grid grid3'+(index+1)+'">'+board[2][index]+'</div>'
+            row1.innerHTML += '<div class="grid" id="grid0'+(index)+'">'+board[0][index]+'</div>'
+            row2.innerHTML += '<div class="grid" id="grid1'+(index)+'">'+board[1][index]+'</div>'
+            row3.innerHTML += '<div class="grid" id="grid2'+(index)+'">'+board[2][index]+'</div>'
         }
+        drawBoard();
+    }
+    const drawBoard = () => {
+        const grids = document.querySelectorAll('.grid');
+        grids.forEach(grids => {
+            grids.addEventListener(
+                'click',
+                () => {
+                    let gridid = grids.getAttribute('id');
+                    board[gridid[4]][gridid[5]] = player2.symbol;
+                    render();
+                }
+            )
+        });
     }
     return {render};
 }
 )();
-
-
 
 gameFlow.render();
